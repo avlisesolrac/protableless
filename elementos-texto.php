ffd8c45997c4f08eab5af277d6e0ac84f6a39071
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 19:28:12 -0200

    Elementos para formatação de texto na página

---
+++ elementos-texto.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chave separadas por vírgula"/>
+<title>Untitled Document</title>
+</head>
+<body>
+
+	<addr>Indica uma abreviação de palavra</addr><br/>
+    <address>Informar o contato do autor do artigo</address><br/>
+    <blockquote>Indica uma citação</blockquote><br/>
+    <cite>Indicar o autor da citação</cite><br/>
+    <code>Indicar um script ou código</code><br/>
+    <div>Define um bloco relacionado a formatação</div><br/>
+    <em>Deixa um texto com ênfase</em><br/>
+    <h1>Um cabeçalho mais importante</h1> ao
+    <h6>Um cabeçalho menos importante</h6>
+    <pre>Texto pré definido</pre>
+    <span>conteúdo genérico, Especificamente para definir um estilo</span><br/>
+    <strong>Dar ênfase forte ao navegador e ao trecho do texto
+
+<div id="conteudo" style="width:500px;">
+    <h1>UpInside Tecnologia lança o curso mais completo da web sobre Tableless	<h1>
+    <h2>Essa semana foi lançado no campus da upinside o curso Pro Tableless, o curso aborda de forma completa HTML, CSS, SEO e padrões web.</h2>
+    
+    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos</p>
+    
+    <p>Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</p>
+    
+    <blockquote><cite>O Site Campus UpInside</cite>Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).</blockquote>
+    
+    <!--Colocando um texto dentro ênfase e também dentro de strong é uma técnica de SEO para dar mais importância à um texto na página-->
+    <p><em>O curso pode ser encontrado no site da empresa UpInside Tecnologia<strong>R$159,00</strong>Valor esse que se torna muito barato em vista de seu conteúdo.</em></p>
+    
+    <h3>Entre em contato agora mesmo pelo endereço<address>www.campusup.com.br<address> adquira o seu</h3>
+</div>
+    
+
+</body>
+</html>
