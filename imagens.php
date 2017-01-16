73a55a84fc279e23443455db32f92df183d397e9
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 20:57:25 -0200

    Trabalhando com imagens na página

---
+++ imagens.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chave separadas por vírgula">
+<title>Trabalhando com imagens</title>
+</head>
+<body>
+
+<!--Padrão Web: colocar uma borde="0" para não aparecer uma borda no IE-->
+<a href="#">
+<img tittle="Mensagem que dará significado para o google" alt="Descrever a image com uma mensagem para deficiente visual" src="../php/imagens/logo.jpg" border="0" width="95" height="54"/>
+</a>
+
+</body>
+</html>
