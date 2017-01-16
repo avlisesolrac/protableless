4017e63c14bf5098de85ade9e1c5518bc3f8a624
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 17:58:09 -0200

    Listas ordenadas, não ordenadas e listas de descrição

--- listas.php
+++ listas.php
@@ -3,8 +3,38 @@
 <head>
 <meta charset="utf-8">
 <title>Listas Ordenadas e Não ordenadas</title>
+<meta name="description" content="Descrição da página">
+<meta name="keywords" content="Palavras chave separadas por vírgula">
 </head>
-
 <body>
+
+	<ul>
+    	<li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+    
+    <ol>
+    	<li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ol>
+    
+    <dl>
+    	<dt><strong>Título do artigo 1</strong><dt>
+        <dd>Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</dd>
+        
+        <dt><strong>Título do artigo 2</strong><dt>
+        <dd>Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</dd>
+        
+        <dt><strong>Título do artigo 3</strong><dt>
+        <dd>Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</dd>
+    </dl>
+    
+    <dl>
+    	<dt><strong>Outra lista de descrição</strong></dt>
+        <dd>Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</dd>
+    </dl>
+
 </body>
 </html>
