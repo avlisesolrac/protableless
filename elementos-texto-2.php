8ce38085d71017a922c6e8f90294e431a0f99fa6
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 19:37:34 -0200

    Elementos de formatação de texto 2

---
+++ elementos-texto-2.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Descrição da página">
+<meta name="keywords" content="Palavras separadas por vírgula">
+<title>Extensões de Texto</title>
+</head>
+<body>
+
+<div id="conteudo" style="width:500px">
+	<p>
+    É um fato conhecido de todos que um <b>leitor se distrairá</b> com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar <big>Lorem Ipsum</big> é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível.
+    </p>
+    
+    <hr/>
+    
+    <p>
+   <i>Existem muitas variações</i> disponíveis de passagens de Lorem Ipsum, mas a maioria sofreu algum tipo de alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco convincentes. Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito escondido no meio do texto. Todos os geradores de Lorem Ipsum na internet tendem a repetir pedaços predefinidos conforme necessário, fazendo deste o primeiro <small>gerador</small> de Lorem Ipsum autêntico da internet. Ele usa um dicionário com mais de <sup>2</sup>00 palavras em Latim combinado com um punhado de <sub>modelos</sub> de estrutura de frases para gerar um <del datetime="2017-01-16">Lorem Ipsum com aparência</del> <ins datetime="2017-01-16">Conteúdo aqui, conteúdo aqui</ins> razoável, livre de repetições, inserções de humor, palavras não características, etc.
+    </p>
+    
+</div>
+
+</body>
+</html>
