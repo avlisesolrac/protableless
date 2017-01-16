327e3d54a234dac25042b8bff075ab1c4be555fe
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 21:04:06 -0200

    Referenciando arquivo de estilo externo

---
+++ stylesheet.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style.css" type="text/css" title="Estilo do Site"/>
+<title>Módulo StyleSheet</title>
+</head>
+<body>
+
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+</body>
+</html>
