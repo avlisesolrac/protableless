f9ef02ef8417adfafb1fee395aa3e4c72ce74dce
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 21:30:54 -0200

    Criado arquivo de estilo externo para aplicar as propriedades do CSS nos elementos HTML

---
+++ css-seletores-comandos-elementos.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style.css" type="text/css" title="Estilo do Site"/>
+<title>Módulo StyleSheet</title>
+</head>
+<body>
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+	<div id="conteudo">
+    	<h1>H1 - Título</h1>
+        <h2>H2 - Nosso sub titulo</h2>
+        	<p>P - Resumo do artigo</p>
+            <p><em> Texto representado por EM</em></p>
+            <p><strong> Texto representado por STRONG</strong></p>
+            
+            	<a href="#" title="Link cego">A - Link cego</a>
+                
+                <div class="formato">
+                	<p> P - Texto representado por P dentro da class Formato</p>
+                </div><!--Fecha o formato-->
+    </div><!--Fecha o conteúdo-->
+    
+    <div class="formato">
+        <p> P - Texto representado por P dentro da class Formato</p>
+    </div><!--Fecha o formato-->
+
+</body>
+</html>
