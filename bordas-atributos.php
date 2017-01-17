67da92f3f9affc4aca019dcf2a1cef5535f1c719
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-16 23:25:29 -0200

    Aplicando estilo de bordas nos elementos html

---
+++ bordas-atributos.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style4.css" type="text/css" title="Estilo do Site"/>
+<title>Unidades de Tamanho Relativo</title>
+</head>
+<body>
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+	<div id="caixa">
+		<div class="conteudo">
+			Nosso conteúdo
+		</div><!--Fecha o conteudo-->
+    </div><!--Fecha a caixa-->
+
+</body>
+</html>
