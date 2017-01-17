2769cf4c063da9e1b4fe29a71318ae1f0993f02b
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-17 12:39:03 -0200

    Trabalhando com estilos de fontes na página

---
+++ fontes-tipograficas.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style10.css" type="text/css" title="Estilo do Site"/>
+<title>Formatação Visual - 2</title>
+</head>
+<body>
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+	<div id="caixa">
+    	Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.
+    </div><!--/caixa-->
+
+</body>
+</html>
