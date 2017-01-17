f2407a07dc7d9b1da7fd83a3f96c20bdf3e29a7c
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-17 11:11:19 -0200

    Aplicando as propriedades do background na página

---
+++ cores-fundo.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style9.css" type="text/css" title="Estilo do Site"/>
+<title>Formatação Visual - 2</title>
+</head>
+<body>
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+	<div id="caixa">
+    	Cores de Fundo com CSS
+    </div><!--/caixa-->
+
+</body>
+</html>
