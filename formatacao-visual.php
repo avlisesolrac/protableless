cc7761b10ddcd10ad7c37ad5625f566fc232f38a
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-17 08:11:13 -0200

    Posicionamento dos elementos na página

---
+++ formatacao-visual.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style6.css" type="text/css" title="Estilo do Site"/>
+<title>Formatação Visual</title>
+</head>
+<body>
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+<div id="um">
+	<div id="dois">
+	
+	</div>
+
+	<div id="tres">
+	
+	</div>
+
+	<div id="quatro">
+		
+	</div>
+
+	<div id="clear"></div>
+</div>
+
+
+</body>
+</html>
