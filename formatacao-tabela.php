e09b858313ff4993c724db15790d1d3332f88b93
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-17 15:49:16 -0200

    Aplicando estilo css em tabela

---
+++ formatacao-tabela.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content="Descrição da página"/>
+<meta name="keywords" content="Palavras chaves separadas por vírgula"/>
+<link rel="stylesheet" href="./style12.css" type="text/css" title="Estilo do Site"/>
+<title>Formtação de Texto</title>
+</head>
+<body>
+<!-- Na TAG link é referenciado o arquivo que terá o estilo CSS da página -->
+
+	<table width="500" border="0" cellspacing="0" cellpadding="0">
+		<tr class="titulo">
+			<td>Nome:</td>
+			<td>E-mail:</td>
+			<td align="center">Ação:</td>
+		</tr>
+		<tr>
+			<td>Robson V. Leite</td>
+			<td>contato@upinside.com.br</td>
+			<td align="center"><a href="#" title="Excluir cliente">Excluir</a></td>
+		</tr>
+
+		<tr>
+			<td>Fulano dos Santos</td>
+			<td>fulano@upinside.com.br</td>
+			<td align="center"><a href="#" title="Excluir cliente">Excluir</a></td>
+		</tr>
+
+		<tr>
+			<td>Beltrano da Silva</td>
+			<td>beltrano@upinside.com.br</td>
+			<td align="center"><a href="#" title="Excluir cliente">Excluir</a></td>
+		</tr>
+	</table>
+</body>
+</html>
