543b871e8dcfdb002b87c8a2422b0ef7cda07c60
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-17 23:01:17 -0200

    Criado o menu do print

--- print/index.php
+++ print/index.php
@@ -4,10 +4,35 @@
 <meta charset="utf-8">
 <title>Pro Tableless - Imprima</title>
 <link href="style.css" rel="stylesheet" type="text/css">
+<meta name="description" content="Descrição">
+<meta name="keywords" content="Palavras chaves">
 </head>
 <body>
 
+<div id="site">
+	<div id="header">
+		<div class="logo">
+			<a href="index.php" title="Pro Tableless - Imprima | Home">
+				<img src="./imagens/logo.png" border="0" alt="Logo" title="Pro Tableless, imprima o anúncio" width="480" height="109"/>				
+			</a>
+		</div><!--/logo-->
+			<div class="line"></div><!--/line-->
+			<ul class="menu">
+				<li class="iconr"></li>                                                      
+				<li><a href="#" title="Pro Tableless - Imprima | Início">INÍCIO</a></li><li class="disc"></li>
+				<li><a href="#" title="Pro Tableless - Imprima | Quem Somos">QUEM SOMOS</a></li><li class="disc"></li>
+				<li><a href="#" title="Pro Tableless - Imprima | Portifólio">PORTFÓLIO</a></li><li class="disc"></li>
+				<li><a href="#" title="Pro Tableless - Imprima | Serviços">SERVIÇOS</a></li><li class="disc"></li>
+				<li><a href="#" title="Pro Tableless - Imprima | Expediente">EXPEDIENTE</a></li><li class="disc"></li>
+				<li><a href="#" title="Pro Tableless - Imprima | Fale Conosco">FALE CONOSCO</a></li>
+				<li class="iconl"></li> 
+			</ul><!--/menu-->
+	</div><!--/header-->
 
+	<div id="content">
+		Content
+	</div><!--/content-->
+</div><!--/site-->
 
 </body>
 </html>
