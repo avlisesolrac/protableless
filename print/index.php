20c2ad20d09d5241ff20a37f3bbe5f59bb9f5480
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-18 17:59:01 -0200

    Concluído o conteúdo da pagina com o anúncio do Sofá

--- print/index.php
+++ print/index.php
@@ -35,7 +35,55 @@
 
 	<div id="content">
 		<div id="conteudo">
-			Conteúdo
+			<h1 class="titulo">PRO TABLESS - Anúncio de Sofá</h1>
+
+			<div class="item_left">
+				<div class="ref">
+					<h2>Estofado carmo Ref:1058</h2>
+
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+					<ul>
+					    <li>Lorem Ipsum is simply</li>
+					    <li>ever since the 1500s</li>
+					    <li>printing and typesetting</li>
+					</ul>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
+
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+
+					<span>R$: 2.490,00</span>
+
+					<div class="contato">
+						<span>Entre em contato</span>
+						<p>Telefone: (11) 2568.6618 / Smartphone: (11) 96485.6672</p>
+						<p>E-mail: carlos.eduardo-silva@hotmail.com</p>
+					</div><!--/contato-->
+				</div><!--/ref-->
+
+				<ul class="imagens">
+					<li><img src="./midias/01.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/02.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+				</ul>
+			</div><!--/item_left-->
+
+			<div class="item_right">
+				<div class="thumb"><img src="./midias/03.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></div><!--/thumb-->
+				<ul class="gallery">
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+					<li><img src="./midias/04.png" alt="Imagem do anúncio" title="Sofá Ref:1080"></li>
+				</ul><!--/gallery-->
+				
+				<div class="print"><input type="submit" name="print" value="CLIQUE AQUI PAR IMPRIMIR ESTE ANÚNCIO"></div>
+
+			</div><!--/item_right-->
+
 		</div><!--/conteudo-->
 	</div><!--/content-->
 </div><!--/site-->
