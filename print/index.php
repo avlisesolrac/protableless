66a43726f6d3f00456ae605183291363d408c68b
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-01-18 11:33:39 -0200

    Criado o footer da página, mas falta ajustar o terceiro elemento a direita

--- print/index.php
+++ print/index.php
@@ -13,7 +13,7 @@
 	<div id="header">
 		<div class="logo">
 			<a href="index.php" title="Pro Tableless - Imprima | Home">
-				<img src="./imagens/logo.png" border="0" alt="Logo" title="Pro Tableless, imprima o anúncio" width="480" height="109"/>				
+				<img src="./imagens/logo.png" border="0" alt="Logo" title="Pro Tableless, imprima o anúncio" width="480" height="112"/>				
 			</a>
 		</div><!--/logo-->
 			<div class="line"></div><!--/line-->
@@ -29,10 +29,50 @@
 			</ul><!--/menu-->
 	</div><!--/header-->
 
+	<div id="spacer">
+		
+	</div><!--/espacer-->
+
 	<div id="content">
-		Content
+		<div id="conteudo">
+			Conteúdo
+		</div><!--/conteudo-->
 	</div><!--/content-->
 </div><!--/site-->
+
+<div id="clear"></div><!--/clear-->
+
+<div id="footer">
+	<div class="footer_content">
+		<div id="footer_spacer"></div><!--/content-->
+		<div class="line"></div><!--/line-->
+		<ul class="menu">
+			<li class="iconr"></li>
+			<li><a href="#" title="Pro Tableless - Imprima | Início">INÍCIO</a></li><li class="disc"></li>
+			<li><a href="#" title="Pro Tableless - Imprima | Quem Somos">QUEM SOMOS</a></li><li class="disc"></li>
+			<li><a href="#" title="Pro Tableless - Imprima | Portifólio">PORTFÓLIO</a></li><li class="disc"></li>
+			<li><a href="#" title="Pro Tableless - Imprima | Serviços">SERVIÇOS</a></li><li class="disc"></li>
+			<li><a href="#" title="Pro Tableless - Imprima | Expediente">EXPEDIENTE</a></li><li class="disc"></li>
+			<li><a href="#" title="Pro Tableless - Imprima | Fale Conosco">FALE CONOSCO</a></li>
+			<li class="iconl"></li> 
+		</ul><!--/menu-->
+		
+		<ul class="footer_elementos">
+			<li><img src="./imagens/logo_footer.png" border="0" alt="Logo" title="Pro Tableless, imprima o anúncio" width="247" height="91"/></li>
+			<li>
+				<div class="tel">Ligue:</div>
+				<p>(11) 2568-6400</p>
+				<p>(11) 96485-6672</p>
+			</li>
+
+			<li class="right">
+				<div class="mail">Escreva:</div>
+				<p class="min">carlos.eduardo-silva@hotmail.com</p>
+				<p class="min">caarlos.eduardo.silva@gmail.com</p>
+			</li>
+		</ul><!--/ul_logo_tel_mail-->
+	</div><!--/footer_content-->
+</div><!--/footer-->
 
 </body>
 </html>
